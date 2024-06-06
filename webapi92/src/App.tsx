2e354d57125daf55e0af7930563fbcb12aaca496
CChan-2023 <carlos.chancaamal@gmail.com>
2024-06-06 13:28:44 -0500

    ahora si

--- webapi92/src/App.tsx
+++ webapi92/src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import 'tailwindcss/tailwind.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='bg-blue-500 text-white p-4"'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
